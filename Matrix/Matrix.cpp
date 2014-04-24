c88f9335fee5fc08cc1a2fbedcda7b2890313234
DorkMatter <barbaraalbertine@gmail.com>
2014-04-24 16:01:22 +0200

    herp derp my toolchain might be borken

---
+++ Matrix/Matrix.cpp
@@ -0,0 +1,18 @@
+/*
+ * Matrix.cpp
+ *
+ *  Created on: 24/04/2014
+ *      Author: Vixen
+ */
+
+#include "Matrix.h"
+
+Matrix::Matrix() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Matrix::~Matrix() {
+	// TODO Auto-generated destructor stub
+}
+
